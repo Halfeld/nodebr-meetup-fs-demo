90fa71a73293f195a30a2e7eb112e523d01d6397
Igor Luiz <igor@devstacks.com.br>
2018-01-22 01:07:12 -0200

    Demo to sync and async

--- index.js
+++ index.js
@@ -1,24 +1,13 @@
+const fs = require('fs')
 
-const path = require('path')
-const express = require('express')
-const bodyParser = require('body-parser')
-const stylus = require('stylus')
+// Asynchronous read
+fs.readFile('./test.txt', (err, data) => {
+  if (err) return console.error(err)
+  console.log('Asynchronous read: ' + data.toString())
+})
 
-const router = require('./routes')
+// Synchronous read
+var data = fs.readFileSync('./test.txt')
+console.log('Synchronous read: ' + data.toString())
 
-const app = express()
-
-app.use(stylus.middleware(path.join(__dirname, 'public')))
-app.use(express.static(path.join(__dirname, 'public')))
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'pug')
-
-app.use(bodyParser.urlencoded({ extended: false, limit: '5mb' }))
-app.use(bodyParser.json({ limit: '5mb' }))
-app.use(bodyParser.urlencoded({ extended: true, uploadDir: 'public/uploads/', limit: '10mb' }))
-
-app.use(router)
-
-app.listen(5000, () => console.log('Server initialized.'))
-
-module.exports = app
+console.log('Program Ended')
